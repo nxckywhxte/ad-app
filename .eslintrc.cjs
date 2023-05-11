b3adf2b93a5a26dc4e904dcc311ad32e1fe8379a
Nikita Belonogov <crcku.mark00@gmail.com>
2023-05-11 10:28:23 +0300

    add prettier and config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,29 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        // 'plugin:import/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier',
+        'plugin:react-hooks/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+    plugins: ['react-refresh','react-hooks'],
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off'
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        "import/resolver": {
+            typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+        },
+    }
 }
